3be622824264f4f65c3c275a45c1ade75adb66a1
Eric Cameron <ericcameronnj@gmail.com>
2025-05-27 21:46:13 -0400

    Add new f-strings.py file

---
+++ f-strings.py
@@ -0,0 +1,26 @@
+
+
+
+name = "Eric"
+fav_number = 52
+
+print(name)
+print(f"{name}")
+
+print("Hello,",name,"I also like the number",fav_number,".")
+print(f"Hello, {name} I also like the number {fav_number}.")
+
+PI = 3.14159
+
+print(f"The value of Pi is {PI}")
+print(f"The value of Pi to two decimal places is {PI:.2f}")
+print(f"The value of Pi to four decimal places is {PI:.4f}")
+
+sales_tax_nj = 0.06625
+sales_tax_de = 0
+
+print(f"Sales tax in NJ: {sales_tax_nj:.1%}")
+print(f"Sales tax in DE: {sales_tax_de:.1%}")
+
+large_value = 1234567890
+print(f"Large value is {large_value:,}")
