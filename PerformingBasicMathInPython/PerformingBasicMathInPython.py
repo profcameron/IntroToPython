03f4855d471e10bf8cf54421cb8796234a426aba
Eric Cameron <ericcameronnj@gmail.com>
2025-06-07 23:12:40 -0400

    Add folder structure

---
+++ PerformingBasicMathInPython/PerformingBasicMathInPython.py
@@ -0,0 +1,36 @@
+# Example 1: You want the variable total to be equal to 3 plus 5
+total = 3 + 5
+print("Example 1: ", total)
+
+# Example 2: You want the variable due to be equal to 500 minus 50
+due = 500 - 50
+print("Example 2: ", due)
+
+# Example 3: You want the variable total to be equal to number1 plus number2
+# Note: You can re-use the same variable name as in Example 1
+# In the future, we will change these to use input
+number1 = 45
+number2 = 55
+total = number1 + number2
+print("Example 3: ", total)
+
+# Example 4: Create a variable doubleTime and set it equal to double hourlyRate
+hourlyRate = 15
+doubleTime = hourlyRate * 2
+print("Example 4: ", doubleTime)
+
+# Example 5: Create a variable avgGrade and set it equal to totalPoints divided by 3
+totalPoints = 80 + 90 + 70
+avgGrade = totalPoints / 3
+print("Example 5: ", avgGrade)
+
+# Example 6: Calculate your weekly paycheck in a variable called paycheck
+# You were paid 40 hours at $10/hour and 10 overtime hours at $15/hour
+paycheck = 40 * 10 + 10 * 15
+print("Example 6: ", paycheck)
+
+# Example 7: Create a variable totalMoney and set it equal to yourQuarters plus myQuarters multiplied by 0.25
+yourQuarters = 10
+myQuarters = 10
+totalMoney = (yourQuarters + myQuarters) * 0.25
+print("Example 7: ", totalMoney)
